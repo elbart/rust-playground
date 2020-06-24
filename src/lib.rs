e5779e36b1dfce6ec5d5d3ee2d6bd2540517b863
Tim Eggert <tim.eggert@celeraone.com>
2020-06-24 10:48:32 +0200

    simple builder pattern

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,39 @@ use std::rc::Rc;
 #[derive(Debug)]
 struct YakeTarget {
     pub name: String,
-    pub dependencies: Option<Vec<Rc<YakeTarget>>>,
+    pub dependencies: Vec<Rc<YakeTarget>>,
+    pub sub_targets: Vec<Rc<YakeTarget>>,
 }
 
 #[derive(Debug)]
 struct Yake {
-    pub targets: Vec<Rc<YakeTarget>>
+    pub targets: Vec<Rc<YakeTarget>>,
+}
+
+impl YakeTarget {
+    pub fn new(name: String) -> Self {
+        YakeTarget {
+            name,
+            dependencies: Vec::new(),
+            sub_targets: Vec::new(),
+        }
+    }
+
+    pub fn dependency(mut self, dep: Rc<YakeTarget>) -> Self {
+        self.dependencies.push(dep);
+        self
+    }
+
+    pub fn sub_target(mut self, dep: Rc<YakeTarget>) -> Self {
+        self.sub_targets.push(dep);
+        self
+    }
 }
 
 impl Yake {
-    fn new(targets: Vec<Rc<YakeTarget>>) -> Self {
-        Yake {
-            targets: targets
-        }
-    } 
+    pub fn new(targets: Vec<Rc<YakeTarget>>) -> Self {
+        Yake { targets: targets }
+    }
 }
 
 #[cfg(test)]
@@ -25,20 +44,19 @@ mod tests {
 
     #[test]
     fn base_layout() {
-        let t1 = Rc::new(YakeTarget {
-            name: "psql".into(),
-            dependencies: None
-        });
+        let t1 = Rc::new(YakeTarget::new("psql".into()));
+        let t2 = Rc::new(YakeTarget::new("docker".into()).dependency(Rc::clone(&t1)));
 
-        let t2 = Rc::new(YakeTarget {
-            name: "docker".into(),
-            dependencies: Some(vec![t1.clone()])
-        });
+        let t3 = Rc::new(
+            YakeTarget::new("foo".into())
+                .dependency(Rc::clone(&t1))
+                .sub_target(Rc::clone(&t2)),
+        );
 
-        let targets = vec![t1, t2];
+        let targets = vec![t1, t2, t3];
         let y = Yake::new(targets);
 
         println!("{:#?}", y.targets);
-
+        println!("{:#?}", y.targets.get(1).unwrap().dependencies);
     }
 }
